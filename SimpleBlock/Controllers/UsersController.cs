219998fbcf8ff1dfe0497fe3c60cbaa9bc62fccf
dmitry.brusentsov <dmitry.brusentsov@sharp-dev.net>
2024-01-19 15:34:35 +0800

    first commit

---
+++ SimpleBlock/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleBlock.Models;
+
+namespace SimpleBlock.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            UsersModel user = new UsersModel();
+            int age = 10 / user.age;
+            return View();
+        }
+
+        public ActionResult ListUsers() 
+        {
+            List<UsersModel> users = new List<UsersModel>();
+            users.Add(new UsersModel("Tom", "Johnson", 56, false));
+            users.Add(new UsersModel("Mickey", "Johnson", 22, true));
+            users.Add(new UsersModel("Alex", "Johnson", 39, true));
+
+            return View(users);
+        }
+    }
+}
---
+++ SimpleBlock/Models/UsersModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlock.Models
+{
+    public class UsersModel
+    {
+        public string name;
+        public string surName;
+        public int age = 0;
+        public bool hasCar = true;
+
+        public UsersModel(string name, string surName, int age, bool hasCar) 
+        {
+            this.name = name;
+            this.surName = surName;
+            this.age = age;
+            this.hasCar = hasCar;
+        }
+
+        public UsersModel() { }
+    }
+}
